f4922072f53b6ebec4cf3b49eb70744895541f24
hemagarg288 <hemlata.garg@natwest.com>
2024-09-27 15:05:45 +0530

    Update security.tf

--- security.tf
+++ security.tf
@@ -6,20 +6,20 @@ resource "aws_security_group" "var_demo" {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = [var.v_cidr_block]
+#    cidr_blocks = [var.v_cidr_block]
   }
 
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = [var.v_cidr_block]
+ #   cidr_blocks = [var.v_cidr_block]
   }
 
   ingress {
     from_port   = 53
     to_port     = 53
     protocol    = "tcp"
-    cidr_blocks = [var.v_cidr_block]
+  #  cidr_blocks = [var.v_cidr_block]
   }
 }
