3ab764537d80c6ebea357eaffa3d82afbfd7f22b
hemagarg288 <hemlata.garg@natwest.com>
2024-09-27 14:10:28 +0530

    Update server.tf

--- server.tf
+++ server.tf
@@ -16,9 +16,9 @@ resource "aws_instance" "myawsserver" {
   key_name = "hema-key"
 
   tags = {
-    Name = "raman-DevOps-batch-server"
+    Name = "hema-DevOps-batch-server"
     env = "Production"
-    owner = "Raman"
+    owner = "hema"
   }
   provisioner "local-exec" {
     command = "echo The servers IP address is ${self.public_ip} && echo ${self.public_ip} > /tmp/inv"
