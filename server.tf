401621d23fce66059a345777a4064248301527af
hemagarg288 <hemlata.garg@natwest.com>
2024-09-27 15:22:09 +0530

    Update server.tf

--- server.tf
+++ server.tf
@@ -11,7 +11,7 @@ provider "aws" {
 region = "us-east-1"
 }
 resource "aws_instance" "myawsserver" {
-  ami = "ami-037774efca2da0726"
+  ami = "ami-0ebfd941bbafe70c6"
   instance_type = "t2.micro"
   vpc_security_group_ids= [aws_security_group.var_demo.id]
   key_name = "hema-key"
