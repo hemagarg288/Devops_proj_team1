967df344ca8cec8c105cba75e0c38d42f05b7f68
hemagarg288 <hemlata.garg@natwest.com>
2024-09-27 14:54:15 +0530

    Create security.tf

---
+++ security.tf
@@ -0,0 +1,25 @@
+resource "aws_security_group" "var_demo" {
+  name        = "hema-SG"
+  vpc_id      = "vpc-00dae5f3df962676d"
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [var.v_cidr_block]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [var.v_cidr_block]
+  }
+
+  ingress {
+    from_port   = 53
+    to_port     = 53
+    protocol    = "tcp"
+    cidr_blocks = [var.v_cidr_block]
+  }
+}
